17053cd75d8d2fc34c70c593a75c3ce08453768f
Julija Pecerska <pejulia@student.ethz.ch>
2014-05-08 16:37:01 +0200

    Initial version of general files

---
+++ tandemrepeats/paths.py
@@ -0,0 +1,5 @@
+EXECROOT = "/cluster/home02/infk/pejulia/bin"
+DATAROOT = "/cluster/home02/infk/pejulia/project/tandem_repeat/TRD_helper_data"
+DATAROOT2 = "/My/Dataroot/brutus_scratch"
+RESULTROOT = "/Save/my/results/here"
+ROOT = "/Root/of/my/filesystem"
