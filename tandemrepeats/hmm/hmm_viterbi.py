712c5377112e6343e2fed61e0303262696399490
elkeschaper <elke@inf.ethz.ch>
2014-05-22 16:30:26 +0200

    Add docstrings to viterbi algorithm related methods

--- tandemrepeats/hmm/hmm_viterbi.py
+++ tandemrepeats/hmm/hmm_viterbi.py
@@ -17,141 +17,149 @@ dAmbiguous_amino_acid = {"B": {"D":0,"N": 0}, "Z": {"E":0,"Q":0}, "X": {i:0 for 
 lAll_amino_acid = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y'] + list(dAmbiguous_amino_acid.keys())
 
 
-class Viterbi:
-    """ Calculating the most probable sequence of states given a sequence of emissions
-        and a HMM using the Viterbi algorithm """
 
-    def __init__(self, hmm, emission):
+def viterbi(hmm, emission):
 
-        self.hmm = hmm
-        self.emission = emission
+    """ Calculate the most probable sequence of states given a sequence of emissions
+    and a HMM using the Viterbi algorithm
 
-    def viterbi(self):
+    Get local copies of all variables.
+    All probabilities must be given as logarithms
+    Replace Selenocysteine (U) with Cysteine (C), replace Pyrrolysine (O) with Lysine (K)
+    [Reason: Seldom AAs that are not part of standard amino acid substitution models.]
 
-        """ ??? What do I do?
+    Args:
+        hmm (hmm): An instance of the HMM class.
+        emission (sequence): An instance of the Sequence class.
 
-        Get local copies of all variables.
-        All probabilities must be given as logarithms
-        Replace Selenocysteine (U) with Cysteine (C), replace Pyrrolysine (O) with Lysine (K)
-        [Reason: Seldom AAs that are not part of standard amino acid substitution models.]
+    Returns:
+        `most_likely_path` (list of str): The most likely sequence of `hmm` states to
+        emit the sequence.
 
-        Args:
-            self (viterbi instance)
+    Raises:
+        Exception: [None]
 
-        Returns:
-            `most_likely_path` (list of str): The most likely sequence of HMM states to
-            emit the sequence.
+    .. warning:: [None]
+    .. todo:: Adapt docstrings to refactored Viterbi -> Viterbi_path classes.
+    .. todo:: This line of code should be standardized and commented: emission.replace("U", "C").replace("O", "K")
+    .. todo:: read in global variables (lStandard_amino_acid, dAmbiguous_amino_acid,
+            lAll_amino_acid) from data folder.
+    .. todo:: Check: Do you need local copies of all variables?
+    """
 
-        Raises:
-            Exception: [None]
+    emission = emission.replace("U", "C").replace("O", "K")
 
-        .. warning:: [None]
-        .. todo:: Refactor Viterbi class; adapt docstrings accordingly.
-        .. todo:: read in global variables (lStandard_amino_acid, dAmbiguous_amino_acid,
-                lAll_amino_acid) from data folder.
+    states = hmm.states
+    p_0 = {iS: value for iS,value in hmm.p_0.items()}
+    p_e = {iS: {iE: value for iE,value in emission.items()} for iS,emission in hmm.p_e.items()}
+    p_t = {iS: {iT: value for iT,value in transition.items()} for iS,transition in hmm.p_t.items()}
 
-        """
+    if any([(iS not in lAll_amino_acid) for iS in emission]):
+        raise Exception("There is an unknown amino acid in:\n {}\n".format(emission))
 
-        emission = self.emission.replace("U", "C").replace("O", "K")
+    # In case there are ambiguous amino acids in the sequence, calculate the expected frequencies of the AAs that they could stand for
+    # from the emission frequencies of the hmm in the neutral state "N".
+    for iA in dAmbiguous_amino_acid.keys():
+        if iA in emission:
+            total = np.log10(sum(10 ** p_e['N'][iAmbiguous] for iAmbiguous in dAmbiguous_amino_acid[iA].keys()))
+            for iAmbiguous in dAmbiguous_amino_acid[iA].keys():
+                dAmbiguous_amino_acid[iA][iAmbiguous] = p_e['N'][iAmbiguous] - total
 
-        states = self.hmm.states
-        p_0 = {iS: value for iS,value in self.hmm.p_0.items()}
-        p_e = {iS: {iE: value for iE,value in emission.items()} for iS,emission in self.hmm.p_e.items()}
-        p_t = {iS: {iT: value for iT,value in transition.items()} for iS,transition in self.hmm.p_t.items()}
+    logging.debug("p_0: {0}".format(p_0))
+    logging.debug("p_e: {0}".format(p_e))
+    logging.debug("p_t: {0}".format(p_t))
 
-        if any([(iS not in lAll_amino_acid) for iS in emission]):
-	        raise Exception("There is an unknown amino acid in:\n {}\n".format(emission))
+    # Initialisation of the probabilities on the first emitted character
+    if emission[0] in dAmbiguous_amino_acid:
+        path = {iS: {'probability': p_0[iS] , 'path': [iS]} for iS in states}
+        for iS in states:
+            # Calculate the average emission probability of ambiguity chars.
+            # The numerical trick to calculate the average log values in high precision is reused in the next for-loop and described there.
+            lP_emission = [ dAmbiguous_amino_acid[emission[0]][iAmbiguous] + p_e[iS][iAmbiguous] for iAmbiguous in dAmbiguous_amino_acid[emission[0]] ]
+            max_p = max(lP_emission)
+            path[iS]['probability'] += np.log10( sum( [ 10 ** (i - max_p) for i in lP_emission]  ) ) + max_p
 
-        # In case there are ambiguous amino acids in the sequence, calculate the expected frequencies of the AAs that they could stand for
-        # from the emission frequencies of the hmm in the neutral state "N".
-        for iA in dAmbiguous_amino_acid.keys():
-	        if iA in emission:
-	            total = np.log10(sum(10 ** p_e['N'][iAmbiguous] for iAmbiguous in dAmbiguous_amino_acid[iA].keys()))
-	            for iAmbiguous in dAmbiguous_amino_acid[iA].keys():
-	                dAmbiguous_amino_acid[iA][iAmbiguous] = p_e['N'][iAmbiguous] - total
+    else:
+        path = { iS: {'probability': p_0[iS]+p_e[iS][emission[0]] , 'path': [iS]} for iS in states}
+    logging.debug("Path: {0}".format(path))
 
-        logging.debug("p_0: {0}".format(p_0))
-        logging.debug("p_e: {0}".format(p_e))
-        logging.debug("p_t: {0}".format(p_t))
+    # Viterbi on all remaining emitted characters
+    for iE in emission[1:]:
+        logging.debug("Emitted: {0}".format(iE))
 
-        # Initialisation of the probabilities on the first emitted character
-        if emission[0] in dAmbiguous_amino_acid:
-            path = {iS: {'probability': p_0[iS] , 'path': [iS]} for iS in states}
-            for iS in states:
-                # Calculate the average emission probability of ambiguity chars.
-                # The numerical trick to calculate the average log values in high precision is reused in the next for-loop and described there.
-                lP_emission = [ dAmbiguous_amino_acid[emission[0]][iAmbiguous] + p_e[iS][iAmbiguous] for iAmbiguous in dAmbiguous_amino_acid[emission[0]] ]
-                max_p = max(lP_emission)
-                path[iS]['probability'] += np.log10( sum( [ 10 ** (i - max_p) for i in lP_emission]  ) ) + max_p
+        ### Determine next most probable state and its probability ...
+        for iS in states:
+            p = {}
+            for iFormer in states:
+                if iE in dAmbiguous_amino_acid:
+                    ## Calculate the probability of being in state iS and emitting iAmbiguous for any of the AAs that the ambiguous iE stands for.
+                    # Then, average over these probabilities (taking into account the background frequencies of all iAmbiguous)
+                    dP_Former =  {iAmbiguous: probability_of_the_former_state(iFormer, iS, iAmbiguous, p_e, p_t, path, p) for iAmbiguous in dAmbiguous_amino_acid[iE].keys()}
+                    dP_Former = {iAmbiguous:j for iAmbiguous,j in dP_Former.items() if j}
+                    if len(dP_Former) > 0:
+                        # Next, we need to calculated a weighted average over log10 probabilities.
+                        # For this purpose, we need to transform the log10ps back to ps.
+                        # However, the ps might have too small values. We apply a numerical trick:
+                        # Instead of sum(w(i)p(i)) = sum ( 10 ** ( log(w(i)) + log(p(i)) ) ) we calculate
+                        # sum(w(i)p(i)) = sum ( 10 ** ( log(w(i)) + log(p(i)) + k ) ) / (10 ** k), which can easily be shown to be equivalent.
+                        # For (-k), we choose the maximum value in log(w(i)) + log(p(i)), as it corresponds to the maximum and thus most pronounced probability of the sum.
+                        lP_Former = [ i + dAmbiguous_amino_acid[iE][iAmbiguous] for iAmbiguous,i  in dP_Former.items()]
+                        max_p = max(lP_Former)
+                        p[iFormer] =  np.log10( sum( [ 10 ** (i - max_p) for i in lP_Former]  ) ) + max_p
+                else:
+                    p_Former = probability_of_the_former_state(iFormer, iS, iE, p_e, p_t, path, p)
+                    if p_Former:
+                        p[iFormer] = p_Former
 
-        else:
-            path = { iS: {'probability': p_0[iS]+p_e[iS][emission[0]] , 'path': [iS]} for iS in states}
-        logging.debug("Path: {0}".format(path))
+            ## This error will occur when it is not possible to enter a state iS at emission iE. This can happen for more complex HMMs, but should not happen for the simple
+            ## circular HMMs that we consider here. It means that state iS cannot be reached by emission iE. Either iS cannot emit iE, or iS cannot be reached by transitions at this point.
+            if p == {}:
+                logging.error("The dict p is empty. This should never happen.")
+            # Detect the most likely path.
+            next_state, p_next_state = max(p.items(), key=operator.itemgetter(1))
+            path[iS]['probability_next'] = p_next_state
+            path[iS]['path_next'] = path[next_state]['path'][:]
+            path[iS]['path_next'].append(iS)
 
-        # Viterbi on all remaining emitted characters
-        for iE in emission[1:]:
-            logging.debug("Emitted: {0}".format(iE))
+        ### ... and update the path accordingly
+        for iV in path.values():
+            iV['path'] = iV['path_next'][:]
+            iV['probability'] = iV['probability_next']
+        #logging.debug("Path: {0}".format(path))
 
-            ### Determine next most probable state and its probability ...
-            for iS in states:
-                p = {}
-                for iFormer in states:
-                    if iE in dAmbiguous_amino_acid:
-                        ## Calculate the probability of being in state iS and emitting iAmbiguous for any of the AAs that the ambiguous iE stands for.
-                        # Then, average over these probabilities (taking into account the background frequencies of all iAmbiguous)
-                        dP_Former =  {iAmbiguous: self.probability_of_the_former_state(iFormer, iS, iAmbiguous, p_e, p_t, path, p) for iAmbiguous in dAmbiguous_amino_acid[iE].keys()}
-                        dP_Former = {iAmbiguous:j for iAmbiguous,j in dP_Former.items() if j}
-                        if len(dP_Former) > 0:
-                            # Next, we need to calculated a weighted average over log10 probabilities.
-                            # For this purpose, we need to transform the log10ps back to ps.
-                            # However, the ps might have too small values. We apply a numerical trick:
-                            # Instead of sum(w(i)p(i)) = sum ( 10 ** ( log(w(i)) + log(p(i)) ) ) we calculate
-                            # sum(w(i)p(i)) = sum ( 10 ** ( log(w(i)) + log(p(i)) + k ) ) / (10 ** k), which can easily be shown to be equivalent.
-                            # For (-k), we choose the maximum value in log(w(i)) + log(p(i)), as it corresponds to the maximum and thus most pronounced probability of the sum.
-                            lP_Former = [ i + dAmbiguous_amino_acid[iE][iAmbiguous] for iAmbiguous,i  in dP_Former.items()]
-                            max_p = max(lP_Former)
-                            p[iFormer] =  np.log10( sum( [ 10 ** (i - max_p) for i in lP_Former]  ) ) + max_p
-                    else:
-                        p_Former = self.probability_of_the_former_state(iFormer, iS, iE, p_e, p_t, path, p)
-                        if p_Former:
-                            p[iFormer] = p_Former
+    # Which overall path is the most likely?
+    path_summary = {iS: path[iS]['probability'] for iS in states}
+    most_likely_terminal_state, p_most_likely_path = max(path_summary.items(), key=operator.itemgetter(1))
+    most_likely_path = path[most_likely_terminal_state]['path']
+    logging.debug("The most likely path is {0}. It has a score of {1}.".format(most_likely_path, p_most_likely_path))
 
-                ## This error will occur when it is not possible to enter a state iS at emission iE. This can happen for more complex HMMs, but should not happen for the simple
-                ## circular HMMs that we consider here. It means that state iS cannot be reached by emission iE. Either iS cannot emit iE, or iS cannot be reached by transitions at this point.
-                if p == {}:
-                    logging.error("The dict p is empty. This should never happen.")
-                # Detect the most likely path.
-                next_state, p_next_state = max(p.items(), key=operator.itemgetter(1))
-                path[iS]['probability_next'] = p_next_state
-                path[iS]['path_next'] = path[next_state]['path'][:]
-                path[iS]['path_next'].append(iS)
+    return(most_likely_path)
 
-            ### ... and update the path accordingly
-            for iV in path.values():
-                iV['path'] = iV['path_next'][:]
-                iV['probability'] = iV['probability_next']
-            #logging.debug("Path: {0}".format(path))
+def probability_of_the_former_state(iFormer, iS, iE, p_e, p_t, path, p):
 
-        # Which overall path is the most likely?
-        path_summary = {iS: path[iS]['probability'] for iS in states}
-        most_likely_terminal_state, p_most_likely_path = max(path_summary.items(), key=operator.itemgetter(1))
-        most_likely_path = path[most_likely_terminal_state]['path']
-        logging.debug("The most likely path is {0}. It has a score of {1}.".format(most_likely_path, p_most_likely_path))
+    ''' Calculate the probability of iFormer, given iS and iE together with the dicts of emission and transition probabilities.'''
+    ## Exclude paths with a zero probability (i.e. a log(probability) set to None)
+    if  iS in p_t[iFormer] and None is not p_t[iFormer][iS] and iE in p_e[iS] and None is not p_e[iS][iE] and None is not path[iFormer]['probability']:
+        return path[iFormer]['probability'] + p_t[iFormer][iS] + p_e[iS][iE]
 
-        return(most_likely_path)
-
-    def probability_of_the_former_state(self, iFormer, iS, iE, p_e, p_t, path, p):
-
-        ''' Calculate the probability of iFormer, given iS and iE together with the dicts of emission and transition probabilities.'''
-        ## Exclude paths with a zero probability (i.e. a log(probability) set to None)
-        if  iS in p_t[iFormer] and None is not p_t[iFormer][iS] and iE in p_e[iS] and None is not p_e[iS][iE] and None is not path[iFormer]['probability']:
-            return path[iFormer]['probability'] + p_t[iFormer][iS] + p_e[iS][iE]
-
-        return None
-
+    return None
 
 
 def distance_index(i, j, length):
+    """ Helper function to calculate the distance between two indices in a circular HMM.
+
+        Args:
+            i (int): index i
+            j (int): index j
+            length (int): length of the HMM
+
+        Returns:
+            (int): The distance between the indices `i` and `j`: As the HMM is circular,
+                 `j` may have a smaller value than `i`, even though `j` is ahead of `i`.
+
+        .. todo:: May be replaced by a simple mod, as distance_index is used only once in
+            the code at current.
+    """
     if j > i:
         return j-i
     else:
@@ -160,7 +168,8 @@ def distance_index(i, j, length):
 
 def hmm_path_to_maximal_complete_tandem_repeat_units(lSequence, lPath, lD, alpha = None):
 
-    ''' Convert several viterbi paths of a hmm on several sequences into the corresponding hmm units.
+        """ Convert several viterbi paths of a hmm on several sequences into the corresponding hmm units.
+
         Be ungreedy: Start from the last index in the cluster of all start state and end state indices.
 
         Only integrate repeat units that are at least alpha complete (be it before of after)
@@ -168,7 +177,27 @@ def hmm_path_to_maximal_complete_tandem_repeat_units(lSequence, lPath, lD, alpha
         change this in two occurrences of alpha.
 
         Assume that all states are counted starting on 1.
-    '''
+
+        Args:
+            lSequence (list of str): A list of sequences.
+            lPath (list of list of str): A list of Viterbi paths
+            lD (int): length of the HMM used to create the Viterbi paths.
+
+        Kwargs:
+            alpha (Float): alpha element [0,1].
+
+        Returns:
+            `lMSA`: A list of multiple sequence alignments (MSAs), that is
+                (list of (list of str))
+
+        Raises:
+            Exception: If alpha is not in [0,1].
+
+        .. warning:: [None].
+        .. todo:: Summarize functions related to viterbi in one class. Discuss how!
+        .. todo:: Should the sequences be sequence instances instead of just strings?
+
+        """
 
     if not alpha:
         alpha = 0.6
@@ -276,9 +305,7 @@ def hmm_path_to_maximal_complete_tandem_repeat_units_old(lSequence, lPath, lD, a
             Exception: If alpha is not in [0,1].
 
         .. warning:: [None].
-        .. todo:: Summarize functions related to viterbi in one class. Discuss how!
-        .. todo:: Should the sequences be sequence instances instead of just strings?
-
+        .. todo:: method names old - check whether it can be erased.
         """
 
     if not (alpha >=0 and alpha <= 1):
