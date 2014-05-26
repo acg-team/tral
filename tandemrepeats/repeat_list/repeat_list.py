7ee30d22e4b209208335b010c767dbf375ee3c42
elkeschaper <elke@inf.ethz.ch>
2014-05-26 16:42:15 +0200

    Initialize the Repeat_list class and add a first simple filter function based on the pValue only

--- tandemrepeats/repeat_list/repeat_list.py
+++ tandemrepeats/repeat_list/repeat_list.py
@@ -24,15 +24,30 @@ class Repeat_list:
 
 
 ### FILTER METHODS
-# Needs documentation
-# Requirement:
-#    First input is a (list of `Repeat`).
+# Filter methods are all defined in the same way: Their first input argument is a list of
+# repeats. They return a subset of these repeats, also as a list.
+#   First input is a (list of `Repeat`).
 #   Return is a (list of `Repeat`).
 
 
 def pValue(lRepeat, score, threshold):
+
+    """ Repeat_list filter methods: Repeats with a p-Value below a certain threshold are
+        filtered out.
+
+    __init__ takes HMM parameters (including the alphabet, emission probabilities
+    and transition probabilities) as input, and assigns them to class attributes.
+
+    Args:
+        lRepeat (list of Repeat): The unfiltered list of repeats
+        score (str): The type of score defines the pValue that is used for filtering
+        threshold (float): All repeats with a pValue of type `score` above this threshold
+            are filtered out.
+
+    """
+
     res = []
     for iRepeat in lRepeat:
-        if iRepeat.pValues[score] < threshold:
+        if iRepeat.pValues[score] <= threshold:
             res.append(x)
     return(res)
