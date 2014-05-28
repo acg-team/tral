bd0f016b69c316a1f97a09eee97f68c8693627d7
Julija Pecerska <pejulia@student.ethz.ch>
2014-05-28 12:04:02 +0200

    More tests for hmm read.
    More different tests for the read method, including data file for an hmm
    without an id.

--- tandemrepeats/hmm/test/hmm_io_test.py
+++ tandemrepeats/hmm/test/hmm_io_test.py
@@ -1,11 +1,17 @@
 import logging
 import os
+import pytest
 
 from tandemrepeats.hmm.hmm import HMM
 
 NUM_STATES = 8
 AMINOACIDS = list('ACDEFGHIKLMNPQRSTVWY')
 NUM_AMINOACIDS = len(AMINOACIDS)
+NUM_TRANSITIONS = 7
+
+CARCINUSTATIN_ID = 'PF08261.7'
+WRONG_CARCINUSTATIN_ID = 'PF05932'
+SHORT_CARCINUSTATIN_ID = 'PF08261'
 
 # HMM data dictionary key names
 INSERTION_EMISSIONS = 'insertion_emissions'
@@ -13,14 +19,61 @@ EMISSIONS = 'emissions'
 TRANSITIONS = 'transition'
 COMPO_KEY_NAME = 'COMPO'
 LETTER_KEY_NAME = 'letters'
+ID_KEY_NAME = 'id'
 
-def test_single_hmm_read():
+# Test file names
+TEST_FILE_WITH_ID = 'carcinustatin.hmm'
+TEST_FILE_WITHOUT_ID = 'carcinustatin_no_id.hmm'
 
-    test_dict_list = HMM.read(os.path.join(os.path.abspath('.'), 'hmm', 'test',
-                                           'carcinustatin.hmm'))
+@pytest.fixture
+def path():
+    """Return the path to the test data files.
+    """
+    return os.path.join(os.path.abspath('.'), 'hmm', 'test')
+
+def test_single_hmm_with_id_read(path):
+    test_dict_list = HMM.read(os.path.join(path, TEST_FILE_WITH_ID))
     assert len(test_dict_list) == 1
     test_dict = test_dict_list[0]
 
+    compare_carcinustatin(test_dict)
+
+    assert test_dict[ID_KEY_NAME] == CARCINUSTATIN_ID
+
+
+def test_single_hmm_without_id_read(path):
+    test_dict_list = HMM.read(os.path.join(path, TEST_FILE_WITHOUT_ID))
+    assert len(test_dict_list) == 1
+    test_dict = test_dict_list[0]
+
+    compare_carcinustatin(test_dict)
+
+    assert test_dict[ID_KEY_NAME] is None
+
+
+def test_single_hmm_no_id_with_query_read(path):
+    test_dict_list = HMM.read(os.path.join(path, TEST_FILE_WITHOUT_ID),
+                              id=CARCINUSTATIN_ID)
+    assert len(test_dict_list) == 0
+
+
+def test_single_hmm_with_wrong_query_read(path):
+    test_dict_list = HMM.read(os.path.join(path, TEST_FILE_WITH_ID),
+                              id=WRONG_CARCINUSTATIN_ID)
+    assert len(test_dict_list) == 0
+
+
+def test_single_hmm_with_short_query_read(path):
+    test_dict_list = HMM.read(os.path.join(path, TEST_FILE_WITH_ID),
+                              id=SHORT_CARCINUSTATIN_ID)
+    assert len(test_dict_list) == 1
+    test_dict = test_dict_list[0]
+
+    compare_carcinustatin(test_dict)
+
+    assert test_dict[ID_KEY_NAME] == CARCINUSTATIN_ID
+
+def compare_carcinustatin(test_dict):
     for i in range(0, NUM_STATES):
         assert str(i + 1) in test_dict.keys()
         assert INSERTION_EMISSIONS in test_dict[str(i + 1)].keys()
@@ -30,13 +83,15 @@ def test_single_hmm_read():
             NUM_AMINOACIDS
         assert len(test_dict[str(i + 1)][EMISSIONS]) ==\
             NUM_AMINOACIDS
-        assert len(test_dict[str(i + 1)][TRANSITIONS]) == NUM_STATES - 1
+        assert len(test_dict[str(i + 1)][TRANSITIONS]) == NUM_TRANSITIONS
 
     assert INSERTION_EMISSIONS in test_dict[COMPO_KEY_NAME].keys()
     assert EMISSIONS in test_dict[COMPO_KEY_NAME].keys()
     assert TRANSITIONS in test_dict[COMPO_KEY_NAME].keys()
     assert len(test_dict[COMPO_KEY_NAME][INSERTION_EMISSIONS]) == NUM_AMINOACIDS
     assert len(test_dict[COMPO_KEY_NAME][EMISSIONS]) == NUM_AMINOACIDS
-    assert len(test_dict[COMPO_KEY_NAME][TRANSITIONS]) == NUM_STATES - 1
+    assert len(test_dict[COMPO_KEY_NAME][TRANSITIONS]) == NUM_TRANSITIONS
 
     assert len(set(test_dict[LETTER_KEY_NAME]) ^ set(AMINOACIDS)) == 0
+
+    assert ID_KEY_NAME in test_dict.keys()
