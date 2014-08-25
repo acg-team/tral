40bb145b44fd30366a2ff002850ed797d224bae9
elkeschaper <elke@inf.ethz.ch>
2014-08-25 15:08:06 +0200

    setup.py: Update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     author='Elke Schaper',
     author_email='elke@inf.ethz.ch',
-    packages=['hmm', 'hmm.test'],
+    packages=['hmm', 'hmm.test', 'repeat', 'repeat.test', 'repeat_list', 'repeat_list.test', 'sequence', 'sequence.test'],
     scripts=[],
     url='http://pypi.python.org/pypi/tandemrepeats/',
     license='LICENSE.txt',
@@ -18,4 +18,7 @@ setup(
     install_requires=[
         "numpy >= 1.6.1",
     ],
+    package_data={'tandemrepeats': ['data/*']}
+    package_dir={'tandemrepeats': 'src/tandemrepeats'}
+    scripts=['scripts/detect_tandem_repeats_in_sequence', 'scripts/example_pipeline']
 )
