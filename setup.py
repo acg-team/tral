d6d6d69187fb40ad60799286044e9ba5d40ee0b6
elkeschaper <elke@inf.ethz.ch>
2014-09-10 10:52:15 +0200

    setup.py: Extend

--- setup.py
+++ setup.py
@@ -16,7 +16,18 @@ setup(
         "Programming Language :: Python :: 3",
         ],
     install_requires=[
+        "argparse >= 1.2.1",
+        "Biopython >= 1.64",
+        "docutils >= 0.11",
         "numpy >= 1.6.1",
+        "pytest >= 2.5.2",
+        "scipy >=0.12.0",
+        "setuptools >= 5.1",
+        "Sphinx >= 1.2.2",
+        "XSTREAM >= 1.72",
+        "T-REKS >= 1.3",
+        "HHrepID >= 1.1.0",
+        "TRUST >= 1.0",
     ],
     package_data={'tandemrepeats': ['data/*']}
     package_dir={'tandemrepeats': 'src/tandemrepeats'}
