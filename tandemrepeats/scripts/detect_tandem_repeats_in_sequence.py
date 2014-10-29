f3351b3b2350364f55df6650e84d7f30a0d6b99c
elkeschaper <elke@inf.ethz.ch>
2014-10-29 17:48:34 +0100

    scripts/detect_tandem_repeats_in_sequence.py: detect BUG

--- tandemrepeats/scripts/detect_tandem_repeats_in_sequence.py
+++ tandemrepeats/scripts/detect_tandem_repeats_in_sequence.py
@@ -332,6 +332,9 @@ def refine_denovo(sequences_file, result_file):
                 # Check whether new and old TR overlap. Check whether new TR is significant. If not both, put unrefined TR into final.
                 if not repeat_list.two_repeats_overlap("shared_char", iTR, iTR_refined):
                     rl_tmp = repeat_list.Repeat_list([iTR_refined])
+                    log.error(".")
+                    log.error(iTR_refined.msa)
+                    log.error(iTR.msa)
                     for iB in basic_filter.values():
                         rl_tmp = rl_tmp.filter(**iB)
                     if rl_tmp.repeats:
