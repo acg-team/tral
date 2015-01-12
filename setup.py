d6f18d46909028ad106a4794f6ca89a7edc2a5aa
elkeschaper <elke@inf.ethz.ch>
2015-01-12 21:54:41 +0100

    setup.py: debug for pypi usage

--- setup.py
+++ setup.py
@@ -1,27 +1,33 @@
+import os
 from distutils.core import setup
+
+def read(*paths):
+    """Build a file path from *paths* and return the contents."""
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
 
 setup(
     name='tandemrepeats',
     version='0.3.0',
     author='Elke Schaper',
     author_email='elke.schaper@isb-sib.ch',
-    packages=['hmm', 'hmm.test', 'repeat', 'repeat.test', 'repeat_list', 'repeat_list.test', 'sequence', 'sequence.test'],
-    scripts=['scripts/detect_tandem_repeats_in_sequence', 'scripts/example_pipeline'],
+    packages=['tandemrepeats', 'tandemrepeats.hmm', 'tandemrepeats.hmm.test', 'tandemrepeats.repeat', 'tandemrepeats.repeat.test', 'tandemrepeats.repeat_list', 'tandemrepeats.repeat_list.test', 'tandemrepeats.sequence', 'tandemrepeats.sequence.test'],
+    #packages=find_packages(exclude=['tests*']),
+    scripts=[os.path.join('tandemrepeats', 'scripts', i) for i in ['create_and_annotate_sequence_pickles.py', 'create_hmm_pickles.py', 'detect_tandem_repeats_in_sequence.py', 'example_pipeline.py', 'split_sequence_file.py']],
     url='http://pypi.python.org/pypi/tandemrepeats/',
     license='LICENSE.txt',
     description='Detect and evaluate tandem repeats in genomic sequence data.',
-    long_description=open('README.markdown').read(),
+    long_description=read('README.markdown'),
+    #include_package_data=True, # If you want files mentioned in MANIFEST.in also to be installed...
     classifiers = [
         "Intended Audience :: Science/Research",
         "Intended Audience :: Developers",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
+        'Natural Language :: English',
         "Topic :: Software Development",
         "Topic :: Scientific/Engineering",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: POSIX",
-        "Operating System :: Unix",
-        "Operating System :: MacOS"
+        "Operating System :: OS Independent",
         ],
     install_requires=[
         "argparse >= 1.2.1",
