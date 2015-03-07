3c403f2888d82ca6cd8e54cefe2f935b232fd245
elkeschaper <elke@inf.ethz.ch>
2015-03-07 13:05:30 +0100

    Add configuration paths to setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,38 @@
 import glob
 import os
+import sys
 
 try:
-    from setuptools import setup
+    from setuptools import setup, Command
 except ImportError:
-    from distutils.core import setup
+    from distutils.core import setup, Command
 
 def read(*paths):
     """Build a file path from *paths* and return the contents."""
     with open(os.path.join(*paths), 'r') as f:
         return f.read()
 
-HOME=os.path.expanduser('~')
+# Set the home variable with user argument:
+# Prettier solutions might be possible: http://stackoverflow.com/questions/677577/distutils-how-to-pass-a-user-defined-parameter-to-setup-py
+try:
+    i = sys.argv.index("--home")
+    HOME = sys.argv[i + 1]
+    del sys.argv[i+1]
+    del sys.argv[i]
+    if not os.path.exists(HOME):
+        raise ValueError('The argument supplied in --home is not a valid path: {}'.format(HOME))
+except:
+    HOME=os.path.expanduser('~')
+
 
 setup(
     name='tral',
     version='0.3.0',
     author='Elke Schaper',
     author_email='elke.schaper@isb-sib.ch',
-    packages=['tral', 'tral.hmm', 'tral.hmm.test', 'tral.repeat', 'tral.repeat.test', 'tral.repeat_list', 'tral.repeat_list.test', 'tral.sequence', 'tral.sequence.test'],
+    packages=['tral', 'tral.test', 'tral.hmm', 'tral.hmm.test', 'tral.repeat', 'tral.repeat.test', 'tral.repeat_list', 'tral.repeat_list.test', 'tral.sequence', 'tral.sequence.test'],
     #packages=find_packages(exclude=['tests*']),
-    scripts=[os.path.join('tral', 'scripts', i) for i in ['create_and_annotate_sequence_pickles.py', 'create_hmm_pickles.py', 'detect_tandem_repeats_in_sequence.py', 'example_pipeline.py', 'split_sequence_file.py']],
+    scripts=[os.path.join('tral', 'examples', i) for i in ['create_and_annotate_sequence_pickles.py', 'create_hmm_pickles.py', 'detect_tandem_repeats_in_sequence.py', 'example_pipeline.py', 'split_sequence_file.py']],
     url='http://pypi.python.org/pypi/tral/',
     license='LICENSE.txt',
     description='Detect and evaluate tandem repeats in genomic sequence data.',
@@ -54,3 +66,6 @@ setup(
     package_data={'tral': ['data/*.ini', 'data/paml/*', 'data/hhrepid/*']},
     package_dir={'tral': 'tral'},
 )
+
+
+print("The TRAL configuration files are now located in {}".format(os.path.join(HOME, ".tral")))
