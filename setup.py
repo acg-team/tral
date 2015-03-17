f02c6ed05ba9e89e83b377d8c974de9580d34afd
elkeschaper <elke@inf.ethz.ch>
2015-03-17 11:25:02 +0100

    setup.py: Update package data and required libraries

--- setup.py
+++ setup.py
@@ -25,20 +25,22 @@ try:
 except:
     HOME=os.path.expanduser('~')
 
+SCRIPTS1 = [os.path.join('tral', 'examples', i) for i in ['create_and_annotate_sequence_pickles.py', 'create_hmm_pickles.py', 'detect_tandem_repeats_in_sequence.py', 'example_pipeline.py', 'split_sequence_file.py']]
+SCRIPTS2 = [os.path.join('tral', 'examples', 'workflow', i) for i in ['tandem_repeat_annotation_scripts.py', 'tandem_repeat_annotation_workflow.py']]
 
 setup(
     name='tral',
-    version='0.3.0',
+    version='0.3.11',
     author='Elke Schaper',
     author_email='elke.schaper@isb-sib.ch',
     packages=['tral', 'tral.test', 'tral.hmm', 'tral.hmm.test', 'tral.repeat', 'tral.repeat.test', 'tral.repeat_list', 'tral.repeat_list.test', 'tral.sequence', 'tral.sequence.test'],
     #packages=find_packages(exclude=['tests*']),
-    scripts=[os.path.join('tral', 'examples', i) for i in ['create_and_annotate_sequence_pickles.py', 'create_hmm_pickles.py', 'detect_tandem_repeats_in_sequence.py', 'example_pipeline.py', 'split_sequence_file.py']],
+    scripts= SCRIPTS1 + SCRIPTS2,
     url='http://pypi.python.org/pypi/tral/',
     license='LICENSE.txt',
     description='Detect and evaluate tandem repeats in genomic sequence data.',
     long_description=read('README.markdown'),
-    #include_package_data=True, # If you want files mentioned in MANIFEST.in also to be installed...
+    #include_package_data=True, # If you want files mentioned in MANIFEST.in also to be installed, i.e. copied to usr/local/bin
     classifiers = [
         "Intended Audience :: Science/Research",
         "Intended Audience :: Developers",
@@ -52,21 +54,20 @@ setup(
     install_requires=[
         "Biopython >= 1.64",
         "configobj >= 5.0.6",
-        "docutils >= 0.11",
+        #"docutils >= 0.11", # Uncomment if you wish to create the documentation locally.
         "numpy >= 1.6.1",
         "pytest >= 2.5.2",
         "scipy >=0.12.0",
-        "setuptools >= 5.1",
-        "Sphinx >= 1.2.2",
+        #"Sphinx >= 1.2.2", # Uncomment if you wish to create the documentation locally.
     ],
-    package_data={'tral': ['data/*.ini', 'data/hhrepid/*', 'data/substitution_rate_matrices/*']},
+    package_data={'tral': ['data/*.ini', 'data/data/hhrepid/*', 'data/data/substitution_rate_matrices/*', 'examples/*.py', 'examples/data/*', 'examples/workflow/*']},
     package_dir={'tral': 'tral'},
 )
 
 
 TRAL = os.path.join(HOME, ".tral")
 if os.path.exists(TRAL):
-    print("Beware: The TRAL configuration directory {} already exists. The template configuration and datafiles are not copied to the already existing directory".format(TRAL))
+    print("The TRAL configuration directory {} already exists. The template configuration and datafiles are not copied to the already existing directory at this step.".format(TRAL))
 else:
     shutil.copytree("tral/data", TRAL)
     print("The TRAL configuration files and data files are now located in {}".format(TRAL))
