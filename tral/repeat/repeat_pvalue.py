113a644aa877e93da16b0bcaa162c1169f233b6d
elkeschaper <elke@inf.ethz.ch>
2015-03-22 15:02:15 +0100

    repeat_pvalue.py: auto PEP8

--- tral/repeat/repeat_pvalue.py
+++ tral/repeat/repeat_pvalue.py
@@ -7,7 +7,8 @@ import csv
 import logging
 import numpy as np
 import scipy as sp
-import scipy.stats, scipy.special
+import scipy.stats
+import scipy.special
 
 log = logging.getLogger(__name__)
 
@@ -15,12 +16,12 @@ from tral.paths import *
 
 path_score = join(DATA_DIR, 'pValue')
 
-########################## REPEAT SCORE P-VALUE CALCULATION FUNCTIONS ####################
+########################## REPEAT SCORE P-VALUE CALCULATION FUNCTIONS ####
 
 ##################### phylo & entropy ############################
 
-def empiricalList(l,n, sequence_type = 'AA', score = 'phylo'):
 
+def empiricalList(l, n, sequence_type='AA', score='phylo'):
     """Load and return a numpy list with 10,000 empirical values of the user defined
         distribution from an external file.
 
@@ -54,7 +55,6 @@ def empiricalList(l,n, sequence_type = 'AA', score = 'phylo'):
     .. todo:: Perhaps add distributions beyond lMax, nMax.
     """
 
-
     lMax = 99
     nMax = 49
     if score == 'entropy':
@@ -64,30 +64,40 @@ def empiricalList(l,n, sequence_type = 'AA', score = 'phylo'):
     totalRepeatLengthMax = 1000
     l = min(l, lMax)
     n = min(n, nMax)
-    if l*n > totalRepeatLengthMax:
-        logging.debug("l: %d and n: %d are bigger than the totalRepeatLengthMax: %d" %(l,n,totalRepeatLengthMax))
-        ## Dirty little hack: as we do not have data for this pair of l and n, apply nearest data file.
-        while l*n > totalRepeatLengthMax:
-            if l>n:
+    if l * n > totalRepeatLengthMax:
+        logging.debug(
+            "l: %d and n: %d are bigger than the totalRepeatLengthMax: %d" %
+            (l, n, totalRepeatLengthMax))
+        # Dirty little hack: as we do not have data for this pair of l and n,
+        # apply nearest data file.
+        while l * n > totalRepeatLengthMax:
+            if l > n:
                 l -= 1
             else:
                 n -= 1
 
-    file = join(path_score, sequence_type, score, str(l) + '_' + str(n) + '.npz')
+    file = join(
+        path_score,
+        sequence_type,
+        score,
+        str(l) +
+        '_' +
+        str(n) +
+        '.npz')
     if not os.path.isfile(file):
         raise ValueError("complete pdf file %s does not exist!" % file)
 
     myEmpiricalList = np.load(file)
-    ## It is absolutely necessary to close the numpy filehandle.
-    ## Otherwise, you will have too many open operating system filehandles
-    ## if you run this function many times (more than ulimit -n allows, that is)
+    # It is absolutely necessary to close the numpy filehandle.
+    # Otherwise, you will have too many open operating system filehandles
+    # if you run this function many times (more than ulimit -n allows, that is)
     empiricalList = myEmpiricalList['arr_0']
     myEmpiricalList.close()
 
     return empiricalList
 
-def pValueFromEmpiricialList(myTR, score = 'phylo', myScore = None, empirical = []):
 
+def pValueFromEmpiricialList(myTR, score='phylo', myScore=None, empirical=[]):
     """Calculates the p-Value of a score for the given myTR.
 
 
@@ -104,26 +114,25 @@ def pValueFromEmpiricialList(myTR, score = 'phylo', myScore = None, empirical = 
         pValue: A float.
 
     """
-    if myScore == None:
+    if myScore is None:
         myScore = myTR.score(score)
 
     if len(empirical) == 0:
         empirical = empiricalList(myTR.lD, myTR.n, myTR.sequence_type, score)
 
-    if score in ['entropy']: # A smaller score is a better score for all scores in this list.
+    # A smaller score is a better score for all scores in this list.
+    if score in ['entropy']:
         if myScore == -1:
             return 1
         else:
-            return bisect.bisect_right(empirical, myScore)/len(empirical)
+            return bisect.bisect_right(empirical, myScore) / len(empirical)
     else:
-        return 1 - bisect.bisect_left(empirical, myScore)/len(empirical)
+        return 1 - bisect.bisect_left(empirical, myScore) / len(empirical)
 
 
+########################### pSim & parsimony: read in PDF ################
 
-########################### pSim & parsimony: read in PDF ###########################
-
-def columnPDF(n, score = 'psim', sequence_type = 'AA'):
-
+def columnPDF(n, score='psim', sequence_type='AA'):
     """Load and return the probability density function of the score on random
         ``sequence_type`` data of length ``n``.
 
@@ -157,18 +166,17 @@ def columnPDF(n, score = 'psim', sequence_type = 'AA'):
     .. todo:: Check how this method concurs with the ``empiricalList`` method.
     """
 
-
     # Currently, the pdfs are only save up to nMax = 150
     nMax = 150
-    with open(join(path_score, sequence_type, score, str(min(n,nMax)) + '.txt'), 'r') as pdf_file:
+    with open(join(path_score, sequence_type, score, str(min(n, nMax)) + '.txt'), 'r') as pdf_file:
         pdf = [i for i in csv.reader(pdf_file, dialect='excel-tab')]
         pdf = [np.double(i[0]) for i in pdf[1:]]
         return np.array(pdf)
 
-################################### pSim & parsimony #####################################
+################################### pSim & parsimony #####################
+
 
 def calculate_repeat_structure(myTR):
-
     """ Calculate the number of columns with a certain number of gaps for each column in
     a ``Repeat`` instance.
 
@@ -190,20 +198,26 @@ def calculate_repeat_structure(myTR):
     """
 
     repeatStructure = [
-            len(column.replace("-",""))
-            for column in myTR.msaTD if (2*column.count('-') < len(column))
+        len(column.replace("-", ""))
+        for column in myTR.msaTD if (2 * column.count('-') < len(column))
     ]
     nRepeatStructure = list(set(repeatStructure))
     lRepeatStructure = [
         repeatStructure.count(i) for i in nRepeatStructure
     ]
-    return lRepeatStructure,nRepeatStructure
+    return lRepeatStructure, nRepeatStructure
 
 
-############################ DERIVE p-Value distributions ################################
+############################ DERIVE p-Value distributions ################
 
-def calc_pValues(repeats, resultFilePath, fileName, scoreslist=['phylo','phylo_gap'], gappy_data = False):
-
+def calc_pValues(
+        repeats,
+        resultFilePath,
+        fileName,
+        scoreslist=[
+            'phylo',
+            'phylo_gap'],
+        gappy_data=False):
     """ Create and save a null distribution for ``repeats`` scores for p-Value calculation.
 
     You can use a different null distribution for each column of different length for both
@@ -222,27 +236,37 @@ def calc_pValues(repeats, resultFilePath, fileName, scoreslist=['phylo','phylo_g
 
     # If the repeats are not gappy, than you can use always the same distribution of scores
     # on random data to calculate the p-Value. Otherwise, there might be deletion columns
-    # and the distribution should be loaded each time 'pValueFromEmpiricialList' is called.
+    # and the distribution should be loaded each time
+    # 'pValueFromEmpiricialList' is called.
     empirical_list = []
 
     for iScore in scoreslist:
         if 'parsimony' == iScore:
-            testStatistic = [pValuePars(iRepeat) if iRepeat.lD != 0 else 1 for iRepeat in repeats]
+            testStatistic = [
+                pValuePars(iRepeat) if iRepeat.lD != 0 else 1 for iRepeat in repeats]
         elif 'pSim' == iScore:
-            testStatistic = [pValuePSim(iRepeat) if iRepeat.lD != 0 else 1 for iRepeat in repeats]
+            testStatistic = [
+                pValuePSim(iRepeat) if iRepeat.lD != 0 else 1 for iRepeat in repeats]
         else:
             if not gappy_data:
-                empirical_list = empiricalList(repeats[0].lD, repeats[0].n, repeats[0].sequence_type, iScore)
-            testStatistic = [pValueFromEmpiricialList(iRepeat, iScore, empirical = empirical_list) if iRepeat.lD != 0 else 1 for iRepeat in repeats]
-        score_path = os.path.join(resultFilePath,iScore)
+                empirical_list = empiricalList(
+                    repeats[0].lD,
+                    repeats[0].n,
+                    repeats[0].sequence_type,
+                    iScore)
+            testStatistic = [
+                pValueFromEmpiricialList(
+                    iRepeat,
+                    iScore,
+                    empirical=empirical_list) if iRepeat.lD != 0 else 1 for iRepeat in repeats]
+        score_path = os.path.join(resultFilePath, iScore)
         if not os.path.isdir(score_path):
-                os.makedirs(score_path)
-        print(os.path.join(score_path,fileName))
-        np.savez(os.path.join(score_path,fileName) , np.sort(testStatistic))
+            os.makedirs(score_path)
+        print(os.path.join(score_path, fileName))
+        np.savez(os.path.join(score_path, fileName), np.sort(testStatistic))
 
 
-def dAverageMultinom(l,n, sequence_type, score):
-
+def dAverageMultinom(l, n, sequence_type, score):
     """ Helper function for the analytic calculation of parsimony or pSim scores.
 
     Called by ``dAverageMultipleMaxMultinom`` or ``dAverageMultipleParsMultinom``.
@@ -271,18 +295,18 @@ def dAverageMultinom(l,n, sequence_type, score):
     .. todo:: Describe return value.
     """
 
-    completePDF = columnPDF(n = n, score = score, sequence_type = sequence_type)
+    completePDF = columnPDF(n=n, score=score, sequence_type=sequence_type)
     if l != 1:
         singleColumnPDF = completePDF
-        for i in range(2,l+1):
+        for i in range(2, l + 1):
             completePDF = sp.convolve(completePDF, singleColumnPDF)
     return completePDF
 
-####################################### pSim #############################################
+####################################### pSim #############################
 
 
-def dAverageMultipleMaxMultinom(myTR, precision = 10000.): # python 2: precision must be float
-
+# python 2: precision must be float
+def dAverageMultipleMaxMultinom(myTR, precision=10000.):
     """ Calculate null distribution for e.g. the pSim score for repeats of type ``myTR``
         as a probability density function.
 
@@ -306,47 +330,44 @@ def dAverageMultipleMaxMultinom(myTR, precision = 10000.): # python 2: precision
             CHECK: http://docs.scipy.org/doc/numpy/user/basics.types.html
     """
 
-
-    lRepeatStructure,nRepeatStructure = calculate_repeat_structure(myTR)
+    lRepeatStructure, nRepeatStructure = calculate_repeat_structure(myTR)
 
     p = dAverageMultinom(
         lRepeatStructure[0], nRepeatStructure[0],
-        myTR.sequence_type, score = 'psim'
+        myTR.sequence_type, score='psim'
     )
     val = np.array(
         (np.r_[lRepeatStructure[0]:(lRepeatStructure[0] * nRepeatStructure[0] + 1.)]
-         * (precision/nRepeatStructure[0])).round(),
+         * (precision / nRepeatStructure[0])).round(),
         dtype='uint32'
     )
     if len(lRepeatStructure) == 1:
         # return best values first. for pSim, best = 1, worst = 0
-        return  p[::-1], val[::-1]/(precision*sum(lRepeatStructure))
+        return p[::-1], val[::-1] / (precision * sum(lRepeatStructure))
     else:
-        for i in range(1,len(lRepeatStructure)):
+        for i in range(1, len(lRepeatStructure)):
             p = np.outer(p,
-                dAverageMultinom(
-                    lRepeatStructure[i], nRepeatStructure[i],
-                    myTR.sequence_type, score = 'psim')
+                         dAverageMultinom(
+                             lRepeatStructure[i], nRepeatStructure[i],
+                             myTR.sequence_type, score='psim')
+                         ).ravel()
+
+            val = (val[:, np.newaxis] +
+                   np.array(
+                (np.r_[lRepeatStructure[i]:(lRepeatStructure[i] * nRepeatStructure[i] + 1.)]
+                 * (precision / nRepeatStructure[i])
+                 ).round(),
+                dtype='uint32')
             ).ravel()
 
-            val = (val[:,np.newaxis] +
-                np.array(
-                    (np.r_[lRepeatStructure[i]:(lRepeatStructure[i] * nRepeatStructure[i] + 1.)]
-                        * (precision/nRepeatStructure[i])
-                    ).round(),
-                    dtype='uint32')
-            ).ravel()
-
-
-            x = np.bincount(val,weights=p)
+            x = np.bincount(val, weights=p)
             val = np.unique(val)
             p = x[val]
         # return best values first. for pSim, best = 1, worst = 0
-        return p[::-1], (val/(precision*sum(lRepeatStructure)))[::-1]
+        return p[::-1], (val / (precision * sum(lRepeatStructure)))[::-1]
 
 
 def pValuePSim(myTR):
-
     """ Calculate the p-Value of the pSim score for a Repeat.
 
     Retrieve the probability density function for repeats of the same type as ``myTR``.
@@ -365,31 +386,31 @@ def pValuePSim(myTR):
     .. todo:: Describe ``precision``.
     """
 
-
     precision = 10000.
 
-    pdf = dAverageMultipleMaxMultinom(myTR,precision)
+    pdf = dAverageMultipleMaxMultinom(myTR, precision)
     cumsumPDF = np.cumsum(pdf[0])
 
     #index = np.where(myTR.score('pSim') == pdf[1])[0]
-    index = np.where(np.abs(myTR.score('pSim') - pdf[1]) <= 1./precision)[0]
-    if len(index) == 1: ## standard case: score is included in pdf[0]
+    index = np.where(np.abs(myTR.score('pSim') - pdf[1]) <= 1. / precision)[0]
+    if len(index) == 1:  # standard case: score is included in pdf[0]
         return(cumsumPDF[index[0]])
 
     indices = np.where(myTR.score('pSim') > pdf[1])[0]
     if len(indices) >= 1:
-    ## if pars is not exactly included in list, give back mean of value above
-    ## and value below (should only occur due to numerical imprecision)
+        # if pars is not exactly included in list, give back mean of value above
+        # and value below (should only occur due to numerical imprecision)
         a = min(indices)
-        return((cumsumPDF[a] + cumsumPDF[a-1])/2)
-    else: ## This should only occur if score('pSim') is really bad or really good
+        return((cumsumPDF[a] + cumsumPDF[a - 1]) / 2)
+    # This should only occur if score('pSim') is really bad or really good
+    else:
         return 1
 
-#################################### parsimony #################################
+#################################### parsimony ###########################
 
 
-def dAverageMultipleParsMultinom(myTR, precision = 10000.): # python 2: precision must be float
-
+# python 2: precision must be float
+def dAverageMultipleParsMultinom(myTR, precision=10000.):
     """ Calculate null distribution for the parsimony score for repeats of type ``myTR``
         as a probability density function.
 
@@ -413,40 +434,39 @@ def dAverageMultipleParsMultinom(myTR, precision = 10000.): # python 2: precisio
             CHECK: http://docs.scipy.org/doc/numpy/user/basics.types.html
     """
 
-
-    lRepeatStructure,nRepeatStructure = calculate_repeat_structure(myTR)
+    lRepeatStructure, nRepeatStructure = calculate_repeat_structure(myTR)
 
     p = dAverageMultinom(
         lRepeatStructure[0], nRepeatStructure[0],
-        myTR.sequence_type, score = 'parsimony'
+        myTR.sequence_type, score='parsimony'
     )
     val = np.array(
-        (np.r_[(lRepeatStructure[0]*(nRepeatStructure[0]-1)):-1:-1.]
-        * (precision/(nRepeatStructure[0]-1.))
-        ).round(),
+        (np.r_[(lRepeatStructure[0] * (nRepeatStructure[0] - 1)):-1:-1.]
+         * (precision / (nRepeatStructure[0] - 1.))
+         ).round(),
         dtype='uint32'
     )
     if len(lRepeatStructure) == 1:
         # return best values first. for parsimony, best = 0, worst = 1
-        return  p[::-1], (val/(precision *sum(lRepeatStructure)))[::-1]
+        return p[::-1], (val / (precision * sum(lRepeatStructure)))[::-1]
     else:
-        for i in range(1,len(lRepeatStructure)):
+        for i in range(1, len(lRepeatStructure)):
             try:
                 p = np.outer(p,
-                    dAverageMultinom(
-                        lRepeatStructure[i],
-                        nRepeatStructure[i],
-                        myTR.sequence_type, score = 'parsimony')
-                ).ravel()
+                             dAverageMultinom(
+                                 lRepeatStructure[i],
+                                 nRepeatStructure[i],
+                                 myTR.sequence_type, score='parsimony')
+                             ).ravel()
 
-                val = (val[:,np.newaxis] + np.array(
-                    (np.r_[(lRepeatStructure[i]*(nRepeatStructure[i]-1)):-1:-1.]
-                     * (precision/(nRepeatStructure[i]-1.))
-                    ).round(),
+                val = (val[:, np.newaxis] + np.array(
+                    (np.r_[(lRepeatStructure[i] * (nRepeatStructure[i] - 1)):-1:-1.]
+                     * (precision / (nRepeatStructure[i] - 1.))
+                     ).round(),
                     dtype='uint32')
                 ).ravel()
 
-                x = np.bincount(val,weights=p)
+                x = np.bincount(val, weights=p)
                 val = np.unique(val)
                 p = x[val]
             except:
@@ -457,10 +477,10 @@ def dAverageMultipleParsMultinom(myTR, precision = 10000.): # python 2: precisio
                 return False
         # return best values first. for parsimony, best = 0, worst = 1.
         # Here, the  np.bincount() and np.unique() funs did the reordering.
-        return p, val/(precision*sum(lRepeatStructure))
+        return p, val / (precision * sum(lRepeatStructure))
+
 
 def pValuePars(myTR):
-
     """ Calculate the p-Value of the parsimony score for a Repeat.
 
     Retrieve the probability density function for repeats of the same type as ``myTR``.
@@ -480,32 +500,35 @@ def pValuePars(myTR):
 
     precision = 10000.
 
-    pdf = dAverageMultipleParsMultinom(myTR,precision)
+    pdf = dAverageMultipleParsMultinom(myTR, precision)
     # Check the following three lines:
-    if pdf == False:
+    if not pdf:
         return 1
     cumsumPDF = np.cumsum(pdf[0])
 
     #index = np.where(pdf[1] == myTR.score('parsimony'))[0]
-    index = np.where(np.abs(myTR.score('parsimony') - pdf[1]) <= 1./precision)[0]
-    if len(index) == 1: ## standard case: score is included in pdf[0]
+    index = np.where(
+        np.abs(
+            myTR.score('parsimony') -
+            pdf[1]) <= 1. /
+        precision)[0]
+    if len(index) == 1:  # standard case: score is included in pdf[0]
         return(cumsumPDF[index[0]])
 
     indices = np.where(myTR.score('parsimony') < pdf[1])[0]
     if len(indices) >= 1:
-        ## if pars is not exactly included in list, give back mean of value
-        ## above and value below (should only occur due to numerical imprecision)
+        # if pars is not exactly included in list, give back mean of value
+        # above and value below (should only occur due to numerical
+        # imprecision)
         a = min(indices)
-        return (cumsumPDF[a] + cumsumPDF[a-1])/2
-    else: ## This should only occur if score('parsimony') is really bad
+        return (cumsumPDF[a] + cumsumPDF[a - 1]) / 2
+    else:  # This should only occur if score('parsimony') is really bad
         return 1
 
 
-
-####################################### gap penalty #####################################
+####################################### gap penalty ######################
 
 def gapPenalty(myTR, mu):
-
     """ Calculate the gap penalty for a ``Repeat`` given mutation rate ``mu``.
 
     Args:
@@ -519,6 +542,4 @@ def gapPenalty(myTR, mu):
     .. todo:: Is this function called from anywhere? In case, consider refactoring.
     """
 
-    return ((mu/(1-mu)) ** myTR.nGapStructure) * myTR.pGapStructure
-
-
+    return ((mu / (1 - mu)) ** myTR.nGapStructure) * myTR.pGapStructure
