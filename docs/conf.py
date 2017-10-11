a2b5fdbc4516cc28a374bbe649657891bc00b36b
Elke Schaper <elkewschaper@gmail.com>
2017-10-11 13:07:57 +0200

    Add deploy.sh script for docs

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
     'sphinx.ext.todo',
-    'sphinxcontrib.napoleon',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
