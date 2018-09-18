b6a14ff23233b8a61f3bc0bc6141bfdadd601661
LinaNaef <paulinanaef@hotmail.com>
2018-09-18 16:04:28 +0200

    T-Reks website adaption - changed link - activated link - added --no-check-certificate to wget to prevent security error

--- easy_setup/install_ext_software/treks.sh
+++ easy_setup/install_ext_software/treks.sh
@@ -2,26 +2,6 @@
 
 # INSTALLING T-REKS #####
 # T-REKS: identification of Tandem REpeats in sequences with a K-meanS based algorithm. (https://www.ncbi.nlm.nih.gov/pubmed/19671691)
-# 
-# TODO -- Website (http://bioinfo.montp.cnrs.fr/) is down, any possibility to download it somewhere else? 
-#       Mailed to: julien.jorda@crbm.cnrs.fr & andrey.kajava@crbm.cnrs.fr
-#       Unable to reach Juline Jorda
-#       Tried to get in contact with CRBM (http://www.crbm.cnrs.fr/en/contact/)
-
-# Dear Sir or Madam
-
-# I'm a student at the ZHAW in Switzerland (Applied Computational Life Sciences) and would like to use your Tandem Repeat Detector T-REKS (published 2009) for my Master Thesis.
-
-# Unfortunately, I see no more option to download your program since your (old?) website (http://bioinfo.montp.cnrs.fr) is down.
-# The programe was available with http://bioinfo.montp.cnrs.fr/?r=t-reks.
-# Is there any public version available somewhere else? I wasn't able to find one. Or is the program no longer maintained?
-
-# Many thanks in advance.
-
-
-# Kind regards,
-# Paulina Näf
-
 
 
 ######################
@@ -36,9 +16,8 @@ PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; cd .. ; pwd -P )
 ######################
 ### Installation T-REKS
 
-# ATTTENTION: Not working since the website for downloading is down
-# LINK_TREKS=http://bioinfo.montp.cnrs.fr/t-reks/T-Reks.jar
-# sudo wget $LINK_TREKS -P $TRAL_EXT_SOFTWARE
+LINK_TREKS=https://dali.crbm.cnrs.fr/tools/treks/T-Reks.jar
+wget --no-check-certificate $LINK_TREKS -P $TRAL_EXT_SOFTWARE # no check certificate because otherwise the download will fail
 
 if [ ! -f $TRAL_EXT_SOFTWARE/T-Reks.jar ]; then # test if T-Reks.jar is downloaded or manually put into the directory.
     echo "Something went wrong with downloading T-REKS"
