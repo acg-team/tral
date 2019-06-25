d49b5a0f967cffc9108aa2b3565822ac5143d27c
Spencer Bliven <spencer.bliven@gmail.com>
2019-06-25 21:32:00 +0200

    Take python version from easy_setup configuration

--- easy_setup/setupTRAL.sh
+++ easy_setup/setupTRAL.sh
@@ -13,7 +13,7 @@ shopt -s nocasematch # making comparisons case-insensitive
 set -euo pipefail # exit on errors and undefined vars
 
 if [[ ! "$1" =~ ^(setup|pip)$ ]]; then
-    echo -e "\nPlease provide as argument how you want to install TRAL with setup.py \"setup\" or \"pip\").\n"
+    echo -e "\nPlease provide as argument how you want to install TRAL with setup.py (\"setup\" or \"pip\").\n"
     exit 1
 fi
 
@@ -43,21 +43,22 @@ fi
 
 
 if [[ $1 == "setup" ]]; then
-    
+
     ######################
     ### install tral with setup.py
-    
+
     PARENT_PATH=$(  cd "$(dirname "${BASH_SOURCE[0]}")" || exit $?  ; cd .. ; pwd -P )
-    
+
     # test if TRAL repository is already downloaded
     if [ -d "$PARENT_PATH/tral" ] && [ -e "$PARENT_PATH/setup.py" ] ; then
+        echo -e "\nUsing TRAL source from $PARENT_PATH\n"
 
-        (cd "$PARENT_PATH" && python setup.py install) || {
-            echo -e "\nA problem occured while trying to install TRAL with \"python setup.py install\"."
+        (cd "$PARENT_PATH" && "${PYTHON3:-python3}" setup.py install ${TRAL_SETUP_ARGS:-}) || {
+            echo -e "\nA problem occured while trying to install TRAL with \"${PYTHON3:-python3} setup.py install ${TRAL_SETUP_ARGS:-}\"."
             exit 1
         }
     else
-        
+
         ### installing with git and setup.py
         # if the tral directory is not already downloaded, it has to be cloned from github
         echo -e "\nPlease clone the TRAL repository from github.\n"
@@ -66,7 +67,7 @@ if [[ $1 == "setup" ]]; then
         yn=y
         else read -p "Do you want to clone the TRAL repository from github in "$TRAL_PATH"? yes(y) or no (n):" yn
         fi
-        
+
         case $yn in
             [Yy]* )
                 # check if git is installed
@@ -74,13 +75,13 @@ if [[ $1 == "setup" ]]; then
                     echo "Git is required to clone the repository of TRAL. Otherwise you can download the directory manually." >&2
                     exit 1
                 fi
-                
+
                 git clone https://github.com/acg-team/tral.git "$TRAL_PATH/tral_repository" || {
                     echo -e "\nA problem occured while trying to clone the TRAL repository."
                     exit 1
                 }
-                (cd "$TRAL_PATH/tral_repository" && python setup.py install)  || {
-                    echo -e "\nA problem occured while trying to install TRAL with \"python setup.py install\"."
+                (cd "$TRAL_PATH/tral_repository" && "${PYTHON3:-python3}" setup.py install ${TRAL_SETUP_ARGS:-})  || {
+                    echo -e "\nA problem occured while trying to install TRAL with \"${PYTHON3:-python3} setup.py install ${TRAL_SETUP_ARGS:-}\"."
                     exit 1
                 }
             ;;
@@ -90,34 +91,34 @@ if [[ $1 == "setup" ]]; then
             ;;
         esac
     fi
-    
-    
-    
+
+
+
 elif [[ $1 == "pip" ]]; then
-    
+
     #####################
     ## install tral with pip
     #####################
-    
+
     # -- TODO .tral will not automatically be added to $HOME
-    
+
     {
         echo -e "start installation"
-        "$TRAL_ENV/python3/bin/"${PIP:-pip} install tral|| {
-        
-        echo -e "\nA problem occured while trying to install TRAL with \"pip install\"."
+        "${PIP:-pip}" install tral|| {
+
+        echo -e "\nA problem occured while trying to install TRAL with \"${PIP:-pip} install\"."
         exit $?
         }
-        
+
     } && {
-            
+
         echo -e "\n---------------------------------"
         echo -e "Installing TRAL with pip successful"
-        echo -e "-----------------------------------\n"        
+        echo -e "-----------------------------------\n"
 
     }
-    
-    
+
+
 fi
 
 
@@ -126,16 +127,21 @@ fi
 
 if [ ! -d "$TRAL_CONF/data/pvalue" ]; then
     echo -e  "\nIn order to calculate the p-Value of tandem repeat scores, available p-Value distributions need to be downloaded (2.6Gb) and placed in ~/.tral/data/pvalue."
-    
+
     if [[ "${ACCEPT_ALL:-no}" = "yes" ]]; then
     yn=y
     else read -p "Would you like to do this? yes(y) or no (n):" yn
     fi
-    
+
     case $yn in
         [Yy]* )
             {
-                wget "ftp://ftp.vital-it.ch/papers/vital-it/Bioinformatics-Schaper/pvalue.tar.gz" -P "$TRAL_CONF/data"
+                if [[ ! -f "$TRAL_CONF/data/pvalue.tar.gz" ]]; then
+                    echo "DOWNLOADING"
+                    wget "ftp://ftp.vital-it.ch/papers/vital-it/Bioinformatics-Schaper/pvalue.tar.gz" -P "$TRAL_CONF/data"
+                else
+                    echo "SKIPPING DOWNLOAD"
+                fi
                 tar -xzf "$TRAL_CONF/data/pvalue.tar.gz" -C "$TRAL_CONF/data"
                 rm -rf "$TRAL_CONF/data/pvalue.tar.gz"
                 } || {
