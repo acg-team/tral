d0b09344facdf5589a9e98999913b9172b2fc770
Spencer Bliven <spencer.bliven@gmail.com>
2019-10-18 01:20:35 +0200

    Infrastructure for on-demand downloading of data files
    Individual files can be downloaded with `path.config_file` without
    needing the full pvalue directory.

--- tral/paths.py
+++ tral/paths.py
@@ -1,12 +1,15 @@
 # (C) 2014-2015 Elke Schaper
 
 """
-    :synopsis: The system paths module.
+    :synopsis: Access TRAL configuration files
 
     .. moduleauthor:: Elke Schaper <elke.schaper@isb-sib.ch>
 """
 
 import os
+import shutil
+import urllib.request as request
+from contextlib import closing
 
 # Where are the executables stored? E.g. the tandem repeat detection
 # algorithms?
@@ -21,15 +24,59 @@ CONFIG_DIR = os.path.join(os.path.expanduser('~'), ".tral")
 # Where are the data-files, e.g. for pvalue calculation stored?
 DATA_DIR = os.path.join(CONFIG_DIR, "data")
 
+# URL to download missing config files. If None, don't try to download
+# Corresponds to DATA_DIR contents
+CONFIG_DATA_URL = "ftp://ftp.vital-it.ch/papers/vital-it/Bioinformatics-Schaper/"
 
-def config_file(name):
-    ''' Returns the complete path to the config file `name`.
 
-        Returns the complete path to the config file `name`. Searches first in
-        `CONFIGDIR`. Otherwise, returns the distribution config file.
+def config_file(name, config_dir=CONFIG_DIR, config_url=CONFIG_DATA_URL):
+    '''Returns the complete path to the config file `name`.
+
+    Returns the complete path to the config file `name`.
+
+    Search path:
+        - CONFIG_DIR (~/.tral)
+        - Default files within the tral distribution (PACKAGE_DIRECTORY/tral_configuration/)
+            - .ini files will be copied to CONFIG_DIR
+        - Download from CONFIG_DATA_URL
+
+    Args:
+      - name (str): path to the requested file relative to config_dir. Should be slash-separated (even on windows)
+      - config_dir (str): Override CONFIG_DIR (for testing)
+      - url (str): Override download url (for testing)
     '''
+    # Minimum file size; used to detect download errors
+    MIN_FILE_SIZE = 0
 
-    if os.path.isfile(os.path.join(CONFIG_DIR, name)):
-        return os.path.join(CONFIG_DIR, name)
+    # Config dir
+    user_path = os.path.join(config_dir, name)
+    if os.path.isfile(user_path):
+        return user_path
+
+    # Package
+    path = os.path.join(PACKAGE_DIRECTORY, 'tral_configuration', name)
+    if os.path.isfile(path):
+        # Copy to config_dir
+        shutil.copyfile(path, user_path)
+        return user_path
+
+    # Download
+    path = os.path.join(config_dir, name)
+    url = config_url + name
+    download(url, path)
+    if os.path.isfile(path) and os.path.getsize(path) > MIN_FILE_SIZE:
+        return path
     else:
-        return os.path.join(PACKAGE_DIRECTORY, 'data', name)
+        raise FileNotFoundError("Error downloading %s" % url)
+
+    # Give up
+    raise ValueError("Unknown configuration file: %s" % name)
+
+
+def download(url, path):
+    """Download a url to a file. Overwrites destination if present."""
+
+    with closing(request.urlopen(url)) as r:
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        with open(path, 'wb') as f:
+            shutil.copyfileobj(r, f)
---
+++ tral/test/test_paths.py
@@ -0,0 +1,53 @@
+import os
+import tempfile
+import pytest
+from tral import paths
+from urllib.request import URLError
+
+
+@pytest.fixture
+def config_dir():
+    with tempfile.TemporaryDirectory(prefix="tral-") as config_dir:
+        yield config_dir
+
+    # config_dir automatically cleaned up after yield
+
+
+DISABLED_URL = "http://0.0.0.0/disabled/"  # sentinel non-working URL
+
+
+def test_package_config(config_dir):
+    # Should get copied from package
+    ini_file = paths.config_file('config.ini', config_dir, config_url=DISABLED_URL)
+
+    assert ini_file == os.path.join(config_dir, 'config.ini')
+    assert os.path.exists(ini_file)
+
+    # Not found in package and can't be downloaded
+    with pytest.raises(URLError):
+        ini_file = paths.config_file('foo', config_dir, config_url=DISABLED_URL)
+
+
+def test_download_config(config_dir):
+    small_file = "pvalue/AA/PhyloScore_AA_Empirical_MolecularClock_SameRateOnBranches_10000.descriptor"
+
+    # not in package
+    with pytest.raises(URLError):
+        downloaded = paths.config_file(small_file, config_dir, config_url=DISABLED_URL)
+
+    # can be downloaded
+    downloaded = paths.config_file(small_file, config_dir)
+
+    assert downloaded == os.path.join(config_dir, small_file)
+    assert os.path.exists(downloaded)
+
+    # Check file contents
+    expected = """Phyloscore on random data - AA based on 3mers
+sequenceType: AA
+nTotalRuns: 10000
+batchID: PhyloScore_AA_Empirical_MolecularClock_SameRateOnBranches_10000
+CODEML: Empirical, molecular clock assumption, star tree, constant branch lengths
+SamplesPerRun: 10000"""
+
+    with open(downloaded, 'r') as f:
+        assert f.read() == expected
