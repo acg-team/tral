e80eb8689352d236a68efe56ec110b344f0319c2
Spencer Bliven <spencer.bliven@gmail.com>
2020-06-25 10:43:33 +0200

    Remove python 3.4 tests
    BioPython now requires 3.6. TRAL should still work on 3.4 using older
    BioPython versions, but let's drop official support.

--- setup.py
+++ setup.py
@@ -38,6 +38,8 @@ setup(
         "Intended Audience :: Developers",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Natural Language :: English",
         "Topic :: Software Development",
         "Topic :: Scientific/Engineering",
